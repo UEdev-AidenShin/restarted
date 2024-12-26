b17467f6b92dc13d2bce4e16e8696529cccded15
aiden_shin <shinichi9784@gmail.com>
2024-12-26 15:58:19 +0900

    v1

---
+++ ET1_homework.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//1. 당신의 점수는 몇점입니까? (5명)
+
+	double score[5];
+
+	cout << "1빠 학생은 몇점인가 : ";
+	cin >> score[0];
+
+	cout << "2빠 학생은 몇점인가 : ";
+	cin >> score[1];
+
+	cout << "3빠 학생은 몇점인가 : ";
+	cin >> score[2];
+
+	cout << "4빠 학생은 몇점인가 : ";
+	cin >> score[3];
+
+	cout << "5빠 학생은 몇점인가 : ";
+	cin >> score[4];
+
+	//2. 점수들의 총합은 몇점인가?
+	double totalscore = score[0] + score[1] + score[2] + score[3] + score[4];
+
+	//3. 점수들의 평균은 몇인가?
+	double avgscore = totalscore / 5;
+
+	//4. 당신들의 성적의 총합과 평균은 몇인지 문구 출력하기
+	cout << "평균 : " << avgscore << endl;
+	cout << "총합 : " << totalscore << endl;
+
+	return 0;
+}
